ea6bd772342a645d282ceb2c14e69acc75225346
John P. Doty <jpd@SiO2.(none)>
2012-08-22 20:15:01 -0600

    Flash the green light on REV 2

--- src/bin/NICER-MPU/lse-mpu.c
+++ src/bin/NICER-MPU/lse-mpu.c
@@ -53,9 +53,9 @@ static void blink( void )
 {
 	static unsigned count;
 	if( count < TICK_HZ/2 )
-		PIOA->sodr = 0x00100000;
+		PIOA->sodr = 0x08000000;
 	else
-		PIOA->codr = 0x00100000;
+		PIOA->codr = 0x08000000;
 	count += 1;
 	if( count >= TICK_HZ ) count = 0;
 
@@ -188,7 +188,7 @@ for clarity and to avoid conflict.
 
 	PIOA->asr = 0xc003f800;	/* enable TXD0, RXD0, SPI0 */
 	PIOA->pdr = 0xc0038000;	/* relinquish pins to USART0, SPI0 */
-	PIOA->oer = 0x03300000;	/* enable LED's */
+	PIOA->oer = 0x0c000000;	/* enable LED's */
 	PIOA->sodr = 0x03300000;	/* turn off LED's */
 	PIOA->oer = 0x00000610;		/* bitbang DAC */
 	PIOA->oer = 0x00007800;		/* bitbang SPI0 select bits */
