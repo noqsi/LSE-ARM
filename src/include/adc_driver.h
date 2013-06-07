acfd7123cf61d72d662508496ccda586b0968ad7
John P. Doty <jpd@Chiisana.(none)>
2013-06-06 20:33:52 -0600

    ADC driver compiles

--- src/include/adc_driver.h
+++ src/include/adc_driver.h
@@ -1,8 +1,8 @@
 #include "adc.h"
 
-extern void adc_init( int a );
+void adc_init( struct adc **a, int n );
 extern void adc_sleep( int a );
 extern void adc_stay_awake( int a );
-extern void adc_start( int a, m );
-extern int adc_ready( int a, m );
-extern int adc_fetch( int a, n );
+extern void adc_start( int a, int m );
+extern int adc_ready( int a, int m );
+extern int adc_fetch( int a, int n );
--- src/lib/adc.c
+++ src/lib/adc.c
@@ -4,7 +4,7 @@
  
 #include "adc_driver.h"
 
-static const struct adc *adcs[] = {ADC0, ADC1};
+struct adc **adcs;
 
 #define ADCCLK_HZ 5000000
 
@@ -14,49 +14,54 @@ extern const unsigned mck_hz;
  * Initialize, set speed to max, sleep between conversions
  */
 
-void adc_init( int a )
+void adc_init( struct adc **a, int n )
 {
-	adcs[a].cr = SWRST;
-	adcs[a].mr = ((mck_hz/(2*ADCCLK_HZ))<<PRESCAL_SHIFT) |
+	int i;
+	adcs = a;
+
+	for( i = 0; i < n; i += 1 ) {	
+		adcs[i]->cr = SWRST;
+		adcs[i]->mr = ((mck_hz/(2*ADCCLK_HZ))<<PRESCAL_SHIFT) |
 		(12<<STARTUP_SHIFT) |
 		(3<<SHTIM_SHIFT) |
-		SLEEP;	
+		SLEEP;
+	}
 }
 
 /* Tell ADC to sleep between conversions */
 
 void adc_sleep( int a )
 {
-	adcs[a].mr |= SLEEP;
+	adcs[a]->mr |= SLEEP;
 }
 
 /* Tell ADC to stay awake between conversions */
 
 void adc_stay_awake( int a )
 {
-	adcs[a].mr &= ~SLEEP;
+	adcs[a]->mr &= ~SLEEP;
 }
 
 /* Set channel mask to m, initiate conversion */
 
-void adc_start( int a, m )
+void adc_start( int a, int m )
 {
-	adcs[a].chdr = 0xff;	/* disable 'em all */
-	adcs[a].cher = m;	/* enable what we want */
-	adcs[a].cr = START;
+	adcs[a]->chdr = 0xff;	/* disable 'em all */
+	adcs[a]->cher = m;	/* enable what we want */
+	adcs[a]->cr = START;
 }
 
 /* True if all channels in mask m have converted data available */
 
-int adc_ready( int a, m )
+int adc_ready( int a, int m )
 {
-	return m == (m & adcs[a].sr );
+	return m == (m & adcs[a]->sr );
 }
 
 /* Fetch data from channel n */
 
-int adc_fetch( int a, n )
+int adc_fetch( int a, int n )
 {
-	return adcs[a].cdr[n];
+	return adcs[a]->cdr[n];
 }
 	
